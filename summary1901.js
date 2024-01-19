428beff60c5509efb369043723efac070c778243
callbackKid <vladislav.yarosh23@gmail.com>
2024-01-19 15:48:25 +0400

    form

--- summary1901.js
+++ summary1901.js
@@ -4,6 +4,52 @@ const root = document.querySelector('#root')
 const header = document.createElement('header')
 root.append(header)
 
+// FORM
+const form = document.createElement('form')
+root.append(form)
+
+const nameLabel = document.createElement('label')
+nameLabel.setAttribute('for', 'name')
+nameLabel.textContent = 'имя'
+form.append(nameLabel)
+
+const nameInput = document.createElement('input')
+nameInput.setAttribute('id', 'name')
+form.append(nameInput)
+
+const ageLabel = document.createElement('label')
+ageLabel.setAttribute('for', 'age')
+ageLabel.textContent = 'возраст'
+form.append(ageLabel)
+
+const ageInput = document.createElement('input')
+ageInput.setAttribute('id', 'age')
+form.append(ageInput)
+
+const cityLabel = document.createElement('label')
+cityLabel.setAttribute('for', 'city')
+cityLabel.textContent = 'город'
+form.append(cityLabel)
+
+const cityInput = document.createElement('input')
+cityInput.setAttribute('id', 'city')
+form.append(cityInput)
+
+const submitButton = document.createElement('button')
+submitButton.innerText = 'Log in'
+submitButton.setAttribute('type', 'submit')
+form.append(submitButton)
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  // event - событие
+  // prevent - предотвратить
+  // default = дефолтный по умолчанию
+  // он предотвращает поведение перезагрузки страницы
+  console.log(event)
+})
+
+// USER CONTAINER
 const userContainer = document.createElement('div')
 userContainer.classList.add('user-container')
 root.append(userContainer)
@@ -19,6 +65,7 @@ themeButton.addEventListener('click', function () {
 
 // ОТРИСОВКА ПОЛЬЗОВАТЕЛЕЙ
 const renderUsers = (arrayOfUsers) => {
+  userContainer.innerHTML = ''
   arrayOfUsers.forEach((user) => {
     const userCard = document.createElement('div')
 
@@ -39,3 +86,16 @@ const renderUsers = (arrayOfUsers) => {
 }
 
 renderUsers(users)
+
+// обработчики событий
+// click - нажимаете на кнопку
+// submit - когда делаете сабмит(отправку) форму
+
+// input - когда что-то вводите в инпут
+// change - когда в checkbox/radio нажимаете что-то/ убираете фокус и текстового инпута
+
+// onMouseEnter - когда наводите мышку
+// onMouseLeave - когда убираете курсор
+
+// bubbling всплытие
+// погружение
